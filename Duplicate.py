868010aa148881291ac1d33957d7fde51bc1f2e8
Anjana Rajan <anjanarajan@Anjanas-MacBook-Pro.local>
2024-03-19 22:47:57 +0100

    third

---
+++ Duplicate.py
@@ -0,0 +1,15 @@
+from typing import List
+class solution:
+    def containsDuplicate(self,nums: List[int]) -> bool:
+        hashset = set() # Create a set to store unique numbers
+        for n in nums:
+            if n in hashset:  # If the number is already in the set, it's a duplicate
+                return True
+            else:
+                hashset.add(n) # Add the number to the set if it's not already present in the set
+
+        return False
+    
+solution_instance = solution()
+nums = [1,2,3,4,2]
+print(solution_instance.containsDuplicate(nums))                
