5fe5d664cfbcc7527aa8657f853251af863c4bd8
AnjanaRjn <136754834+AnjanaRjn@users.noreply.github.com>
2024-03-19 22:49:12 +0100

    Update Duplicate.py

--- Duplicate.py
+++ Duplicate.py
@@ -3,7 +3,7 @@ class solution:
     def containsDuplicate(self,nums: List[int]) -> bool:
         hashset = set() # Create a set to store unique numbers
         for n in nums:
-            if n in hashset:  # If the number is already in the set, it's a duplicate
+            if n in hashset:  # If the number is already in the set, it's a duplicate.
                 return True
             else:
                 hashset.add(n) # Add the number to the set if it's not already present in the set
